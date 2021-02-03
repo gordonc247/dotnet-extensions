ce52d6042196b1da1d2034fc4ba2617fddfb1e7e
Gordon Craig <gordon.craig@pipware.net>
2021-02-03 13:31:29 +1100

    Add Object and Type extensions

---
+++ DotNet.Extensions/Object/TypeExtensions.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Reflection;
+
+namespace DotNet.Extensions {
+    public static class TypeExtensions {
+        public static PropertyInfo GetPropertyAccessor(this Type type, string name)
+        {
+            return type.GetProperty(name, BindingFlags.Instance | BindingFlags.Public | BindingFlags.IgnoreCase);
+        }
+
+        public static bool IsNullable(this Type type)
+        {
+            return type.IsGenericType && type.GetGenericTypeDefinition() == typeof(Nullable<>);
+        }
+    }
+}
